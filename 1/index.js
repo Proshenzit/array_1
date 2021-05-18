3d56f1a04b748c6828e2bfd9dfc090b7ed536bff
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-17 23:38:36 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,46 @@
+
+
+
+var names1= ["Hello","roy","zit","farids","rinki","joy"];
+
+var names2= ["Hello","roy","zit","farids","rinki","joy"];
+
+
+
+
+
+ var name=names1.concat(names2);
+
+ console.log(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
